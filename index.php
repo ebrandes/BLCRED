8f85a376881c3368f15754455d525f3194a4293a
ebrandes <eduardocbrandes@gmail.com>
2014-02-24 23:53:41 -0300

    bl cred 24-02 index
    Menus e bloco abaixo do slider

--- index.php
+++ index.php
@@ -1,205 +1,84 @@
-<?php
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="pt-br">
+    <head>
+        <link href="css/estilos.css" rel="stylesheet" type="text/css"/>
+        <link href="css/menu.css" rel="stylesheet" type="text/css"/>
+        <!-- fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Dosis|Bad+Script|Droid+Sans' rel='stylesheet' type='text/css'>
+        <script src="js/jquery.js" type="text/javascript"></script>
+        <script src="js/main.js" type="text/javascript"></script>
+        <title>BL CRED</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+    </head>
+    <body>
+        <div id="topo_wrapper">
+            <div id="topo">
+                <div id="logo">
+                    <div class="logo">
 
-/*
- *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
- *---------------------------------------------------------------
- *
- * You can load different configurations depending on your
- * current environment. Setting the environment also influences
- * things like logging and error reporting.
- *
- * This can be set to anything, but default usage is:
- *
- *     development
- *     testing
- *     production
- *
- * NOTE: If you change these, also change the error_reporting() code below
- *
- */
-	define('ENVIRONMENT', 'development');
-/*
- *---------------------------------------------------------------
- * ERROR REPORTING
- *---------------------------------------------------------------
- *
- * Different environments will require different levels of error reporting.
- * By default development will show errors but testing and live will hide them.
- */
+                    </div>
+                    <div class="descricao_logo">
+                        BL Cred - <span class="cred">Soluções financeiras</span>
+                    </div>
+                </div>
+                <div id="menu">
+                    <?php include('./menu.php') ?>
+                </div>
+            </div>
+        </div>
+        <div id="container_wrapper">
+            <div id="container">
+                <div id="slider">
+                    <img src="imagens/img1.jpg" title="BL CRED" alt="BL CRED" />
+                </div>
+                <div id="bloco_inf">
+                    <div class="inf">
+                    <h2>Nosso time</h2>
+                    <p>I'm a Paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font.
+                        ​</p>
+                    <p>I’m a great place for you to let your users know a little more about you. If you want to delete me just click on me and press delete.</p>
+                    <a href="#" class="btn">MAIS</a>
+                    </div>
+                    
+                    <div class="inf">
+                    <h2>A empresa</h2>
+                    <p>I'm a Paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me and you can start adding your own content and make changes to the font.
+                        ​</p>
+                    <p>I’m a great place for you to let your users know a little more about you. If you want to delete me just click on me and press delete.</p>
+                    <a href="#" class="btn">MAIS</a>    
+                    </div>
+                    
+                    
+                    <div class="contactUs">
+                        <h2>Fale conosco</h2>
+                        <p class="desc_inf">Através desses contatos, você pode falar diretamente conosco.</p>
+                        <p class="tel">TELEFONE</p>
+                        <p class="telNumero">xx 51 xxxx - xxxx / xx 51 xxxx-xxx</p>
+                        <p class="email">EMAIL</p>
+                        <p class="emailTo">xxxxxxxx@blcred.com.br</p>
+                        <p class="email">SYKPE</p>
+                        <p class="emailTo">blcred_brasil</p>
+                    </div>
+                    
+                    
+                    <div class="clear"></div>
+                    
+                    
+                    
+                   </div>
+            </div>
+        </div>
+        <div id="rodape_wrapper">
+            <div id="rodape">
+                <p class="desc_rodape"></p>
+            </div>
+        </div>
 
-if (defined('ENVIRONMENT'))
-{
-	switch (ENVIRONMENT)
-	{
-		case 'development':
-			error_reporting(E_ALL);
-		break;
-	
-		case 'testing':
-		case 'production':
-			error_reporting(0);
-		break;
-
-		default:
-			exit('The application environment is not set correctly.');
-	}
-}
-
-/*
- *---------------------------------------------------------------
- * SYSTEM FOLDER NAME
- *---------------------------------------------------------------
- *
- * This variable must contain the name of your "system" folder.
- * Include the path if the folder is not in the same  directory
- * as this file.
- *
- */
-	$system_path = 'system';
-
-/*
- *---------------------------------------------------------------
- * APPLICATION FOLDER NAME
- *---------------------------------------------------------------
- *
- * If you want this front controller to use a different "application"
- * folder then the default one you can set its name here. The folder
- * can also be renamed or relocated anywhere on your server.  If
- * you do, use a full server path. For more info please see the user guide:
- * http://codeigniter.com/user_guide/general/managing_apps.html
- *
- * NO TRAILING SLASH!
- *
- */
-	$application_folder = 'application';
-
-/*
- * --------------------------------------------------------------------
- * DEFAULT CONTROLLER
- * --------------------------------------------------------------------
- *
- * Normally you will set your default controller in the routes.php file.
- * You can, however, force a custom routing by hard-coding a
- * specific controller class/function here.  For most applications, you
- * WILL NOT set your routing here, but it's an option for those
- * special instances where you might want to override the standard
- * routing in a specific front controller that shares a common CI installation.
- *
- * IMPORTANT:  If you set the routing here, NO OTHER controller will be
- * callable. In essence, this preference limits your application to ONE
- * specific controller.  Leave the function name blank if you need
- * to call functions dynamically via the URI.
- *
- * Un-comment the $routing array below to use this feature
- *
- */
-	// The directory name, relative to the "controllers" folder.  Leave blank
-	// if your controller is not in a sub-folder within the "controllers" folder
-	// $routing['directory'] = '';
-
-	// The controller class file name.  Example:  Mycontroller
-	// $routing['controller'] = '';
-
-	// The controller function you wish to be called.
-	// $routing['function']	= '';
-
-
-/*
- * -------------------------------------------------------------------
- *  CUSTOM CONFIG VALUES
- * -------------------------------------------------------------------
- *
- * The $assign_to_config array below will be passed dynamically to the
- * config class when initialized. This allows you to set custom config
- * items or override any default config values found in the config.php file.
- * This can be handy as it permits you to share one application between
- * multiple front controller files, with each file containing different
- * config values.
- *
- * Un-comment the $assign_to_config array below to use this feature
- *
- */
-	// $assign_to_config['name_of_config_item'] = 'value of config item';
-
-
-
-// --------------------------------------------------------------------
-// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
-// --------------------------------------------------------------------
-
-/*
- * ---------------------------------------------------------------
- *  Resolve the system path for increased reliability
- * ---------------------------------------------------------------
- */
-
-	// Set the current directory correctly for CLI requests
-	if (defined('STDIN'))
-	{
-		chdir(dirname(__FILE__));
-	}
-
-	if (realpath($system_path) !== FALSE)
-	{
-		$system_path = realpath($system_path).'/';
-	}
-
-	// ensure there's a trailing slash
-	$system_path = rtrim($system_path, '/').'/';
-
-	// Is the system path correct?
-	if ( ! is_dir($system_path))
-	{
-		exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
-	}
-
-/*
- * -------------------------------------------------------------------
- *  Now that we know the path, set the main path constants
- * -------------------------------------------------------------------
- */
-	// The name of THIS file
-	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
-
-	// The PHP file extension
-	// this global constant is deprecated.
-	define('EXT', '.php');
-
-	// Path to the system folder
-	define('BASEPATH', str_replace("\\", "/", $system_path));
-
-	// Path to the front controller (this file)
-	define('FCPATH', str_replace(SELF, '', __FILE__));
-
-	// Name of the "system folder"
-	define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
-
-
-	// The path to the "application" folder
-	if (is_dir($application_folder))
-	{
-		define('APPPATH', $application_folder.'/');
-	}
-	else
-	{
-		if ( ! is_dir(BASEPATH.$application_folder.'/'))
-		{
-			exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
-		}
-
-		define('APPPATH', BASEPATH.$application_folder.'/');
-	}
-
-/*
- * --------------------------------------------------------------------
- * LOAD THE BOOTSTRAP FILE
- * --------------------------------------------------------------------
- *
- * And away we go...
- *
- */
-require_once BASEPATH.'core/CodeIgniter.php';
-
-/* End of file index.php */
-/* Location: ./index.php */
+    </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,33 @@
+<div id='cssmenu'>
+<ul>
+   <li class='active'><a href='index.html'><span>Home</span></a></li>
+   <li><a href='#'><span>Empresa</span></a></li>
+   <li class='has-sub'><a href='#'><span>Produtos</span></a>
+      <ul>
+         <li><a href='#'><span>Crédito imobiliário</span></a></li>
+         <li class='has-sub'><a href='#'><span>INSS</span></a>
+            <ul>
+               <li class='last'><a href='#'><span>Aposentados e pensionistas</span></a></li>
+            </ul>
+         </li>
+         <li><a href='#'><span>Ipe</span></a></li>
+         <li><a href='#'><span>Estado</span></a></li>
+         <li><a href='#'><span>Governo</span></a></li>
+         <li class='has-sub'><a href='#'><span>Funcionários federais</span></a>
+            <ul>
+               <li class='last'><a href='#'><span>Aposentados e pensionistas</span></a></li>
+            </ul>
+         </li>
+         <li class='has-sub'><a href='#'><span>Forças armadas</span></a>
+            <ul>
+               <li><a href='#'><span>Exército</span></a></li>
+               <li><a href='#'><span>Marinha</span></a></li>
+               <li class='last'><a href='#'><span>Aeronáltica</span></a></li>
+            </ul>
+         </li>
+      </ul>
+   </li>
+   <li><a href='#'><span>Simluador</span></a></li>
+   <li class='last'><a href='#'><span>Contato</span></a></li>
+</ul>
+</div>
